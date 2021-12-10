0f3a8b1acb25eb37a7f3af4e2e71350e887c40d6
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-12-10 15:27:33 +0100

    Add fetchAccountsByPageNo

--- src/UserDetailsView.tsx
+++ src/UserDetailsView.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {userObj} from './data/data';
-import {accountsObj} from './data/data';
+import {userObjBackup} from './data/data';
+import {accountsObjBackup} from './data/data';
+import axios from 'axios';
 
 interface GetUserResponse {
   email: string;
@@ -23,23 +24,31 @@ export const UserDetailsView = (props: UserDetailsViewProps) => {
   const [pageNumber, setPageNumber] = useState<number>(props.pageNo);
   useEffect(() => {
     const fetchUser = async () => {
-      await setTimeout(() => {
-        setUser(userObj)
-      }, 3000);
+      //użyte mockAPI, gdyby były odpowiednie endpointy powinno być
+      //const accountObj = await axios'/api/user'
+      const userObj = await axios('https://mocki.io/v1/d053a495-3838-41d3-a5ba-cca3704f1fd1');
+      setUser(userObj.data || userObjBackup)
     };
     const fetchAccounts = async () => {
-      await setTimeout(() => {
-        setAccount(accountsObj)
-      }, 3000);
+      //użyte mockAPI, gdyby były odpowiednie endpointy powinno być
+      //const accountObj = await axios'/api/bank-accounts/'
+      const accountsObj = await axios('https://mocki.io/v1/aeffce6f-6165-4c8e-a593-d11e29925a9c');
+      setAccount(accountsObj.data || accountsObjBackup)
+    };
+    const fetchAccountsByPageNo = async () => {
+      const URL = `/api/bank-accounts/${pageNumber}`;
+      //console.log(URL);
+      await axios(URL);
     };
 
     fetchUser();
     fetchAccounts();
+    fetchAccountsByPageNo()
   }, [pageNumber]);
 
   return (
     user === null || account === null ?
-    <h1>Loading</h1>
+    <h1>Loading...</h1>
     :
     <div>
         <h1>User is: {user?.email}</h1>
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,4 @@
-export const userObj =  {email: 'julia@example.com'}
+export const userObjBackup =  {email: 'julia@example.com'}
 
-export const accountsObj =  {accounts: [{balance: 12345}, {balance: 54321}, {balance: 13153}, {balance: 12435}]}
+export const accountsObjBackup =  {accounts: [{balance: 12345}, {balance: 54321}, {balance: 13153}, {balance: 12435}]}
+
